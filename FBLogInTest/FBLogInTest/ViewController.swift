3eec7b864ccbadbced47f197da1548a96f806de1
nelsongonzalezpo <nelson.gonzalezpo@udlap.mx>
2018-10-24 23:04:52 -0500

    Adding NWM Repo Version 1.6.4, this version works Swift 4

---
+++ FBLogInTest/FBLogInTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FBLogInTest
+//
+//  Created by Nelson Gonzalez on 10/15/18.
+//  Copyright © 2018 Nelson Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
